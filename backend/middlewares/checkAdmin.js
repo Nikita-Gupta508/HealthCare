3d1fcc592930a5840510f06ae4b15e44039d57a3
Nikita Gupta <143890264+Nikita-Gupta508@users.noreply.github.com>
2025-09-07 03:42:54 +0530

    Update checkAdmin.js

--- backend/middlewares/checkAdmin.js
+++ backend/middlewares/checkAdmin.js
@@ -4,21 +4,24 @@ const checkAdmin = ()=>(req, res, next) => {
     // Check for token in header or authorization header
     const token = req.header("x-access-token") || req.header("Authorization")?.replace("Bearer ", "");
 
-    if (!token) {
-        return res.status(401).json({ error: "You are not logged in!!!" });
-     }  
+    //if (!token) {
+     //   return res.status(401).json({ error: "You are not logged in!!!" });
+    // }  
      try {
         const verified = jwt.verify(token, process.env.jwtsecret);
-        
+        console.log("1",verified);
         if (!verified) {
+            console.log("2",verified);
             return res.status(401).json({ error: "You are not logged in!!!" });
         }
-
+console.log("3",verified);
         if (verified.role !== 'admin') {
+            console.log("4",verified);
             return res.status(403).json({ error: "You do not have permission to access this resource." });
         }
 
         req.user = verified;
+         console.log("5",req.user);
         next();
     } catch (error) {
         console.error("JWT Error:", error.message);
